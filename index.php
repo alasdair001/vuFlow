7108bad74c324845a0a23033a019592bb9c24a57
Black-Viper <blackviper@reflexpro.co.uk>
2015-12-14 23:18:53 +0000

    Comments

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
+<?php
+	
+	/*
+		
+		
+		File 1 - PHP
+		by Alasdair Gallacher
+		
+		Index page...
+		
+		
+		Languages Used:
+		
+		- HTML, CSS, JS, PHP, MSQL	
+		
+		
+		
+		Please annotate your code!
+		
+	*/	
+	
+	
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>test</title>
 	</head>
+	
 	<body>
 	</body>
 </html>
