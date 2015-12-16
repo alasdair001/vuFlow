ec55d77d4c4055fb7d88169c222a0d4f1f46c3ca
Breeze <billybobserver@gmail.com>
2015-12-16 15:38:33 +0000

    Update login.php

--- api/login.php
+++ api/login.php
@@ -8,86 +8,86 @@
 		
 	*/		
 	
-	include("core.php");
-	import(array("core.database_utils", "crust.database_utils"));
+	include("core.php"); // Include core utils
+	import(array("core.database_utils", "crust.database_utils")); // Use core importing method
 	
-	if(!isset($_POST['api_method'])
+	if(!isset($_POST['api_method']) // Check if authentication method is app or 
 	{
-		echo "CLIENT API CONNECTION ERROR";
-		exit;
+		echo "CLIENT API CONNECTION ERROR"; // Print out the error message
+		exit; // Exit the program
 	}
 	
-	$web_method = $_POST['api_method'] != "APP";
+	$web_method = $_POST['api_method'] != "APP"; // Check if the method is app authentication
 	
-	function pushState($message, $success)
+	function pushState($message, $success) // Create method to exit and push response
 	{
-		db_close();
-		if($web_method)
+		db_close(); // Close connection to database
+		if(!$web_method) // If web method and not app method
 		{
-			echo $message;
-			exit;
+			echo $message; // Just print the response
+			exit; // Exit code
 		}else
 		{
-			if(!$success)
+			if(!$success) // If wasn't successful
 			{
 				setcookie("authen_error", $message, time() + 900); // Create cookie that expires after 15 mins
-				header("Location: /");
+				header("Location: /"); // Redirect to main page
 			}else
 			{
-				header("Location: /");
+				header("Location: /"); // Redirect to main page
 			}			
-			exit;
+			exit; // Exit code
 		}
 	}
 	
-	if(!isset($_POST['user_identifier']))
+	if(!isset($_POST['user_identifier'])) // Check if user_identifier is defined
 	{
-		pushState("You must enter a username/email.", false);
+		pushState("You must enter a username/email.", false); // Push response
 	}
 	
-	if(!isset($_POST['user_password']))
+	if(!isset($_POST['user_password'])) // Check if user_password is defined
 	{
-		pushState("You must enter a password.", false);
+		pushState("You must enter a password.", false); // Push response
 	}
 	
-	$identifier = $_POST['user_identifier'];
-	$password = $_POST['user_password'];
+	$identifier = $_POST['user_identifier']; // Set local variable of identifier
+	$password = $_POST['user_password']; // Set local variable of password
 	
-	if(!db_connect())
+	if(!db_connect()) // If didn't connect to database
 	{
-		pushState("Failed to connect to database.", false);
+		pushState("Failed to connect to database.", false); // Push response
 	}
 	
-	$user = getUserByIdentifier($identifier);
+	$user = getUserByIdentifier($identifier); // Set user to get user by identifier
 	
-	if($user == false)
+	if($user == false) // If user doesn't exist
 	{
-		pushState("Incorrect login details.", false);
+		pushState("Incorrect login details.", false); // Push response
 	}
 	
-	$password = md5($password.$user['salt']);
+	$password = md5($password.$user['salt']); // Encrypt the password using md5
 	
-	$user = getUserByPayload($user['id'], $password);
+	$user = getUserByPayload($user['id'], $password); // Get user by payload (correct login data)
 	
-	if($user == false)
+	if($user == false) // If user doesn't exist
 	{
-		pushState("Incorrect login details.", false);
+		pushState("Incorrect login details.", false); // Push response
 	}else
 	{
-		pushState("Incorrect login details.", false);
+		pushState("Incorrect login details.", false); // Push response
 	}
 	
-	$session_token = createSession($user);
+	$session_token = createSession($user); // Create session token
 	
-	if(!$session_token)
+	if(!$session_token) // If session token
 	{
-		pushState("Failed to create session token.", false);
+		pushState("Failed to create session token.", false); // Push response
 	}
 	
-	if($web_method)
+	if($web_method) // If using web api
 	{
-		setcookie("session_token", $session_token, time() + 300);
+		setcookie("session_token", $session_token, time() + 300); // Set session token
 	}
 	
-	pushState($session_token, true);	
+	pushState($session_token, true); // Push session toeken as response
 ?>
